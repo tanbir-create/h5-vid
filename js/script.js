e2e637d3c8d3490d0389838515912ef92b75ec0d
Tanbir Ahmed Mazumdar <46240243+tanbir-create@users.noreply.github.com>
2022-05-13 15:44:16 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -11,7 +11,7 @@ const shareData = {
 
   // Share must be triggered by "user activation"
   b.addEventListener('click', async () => {
-      window.open(`whatsapp://send?text=${shareData.text}%0a${shareData.url}`);
+      window.open(`https://www.facebook.com/sharer/sharer.php?u=${shareData.url}&t=${shareData.text}`);
 //     try {
 //       await navigator.share(shareData)
      
